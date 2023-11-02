062d0597ae325ac2b84f891dacc8d0d570776247
I571785 <david.joo@sap.com>
2023-11-02 13:55:22 +0100

    custom html, multiple pdfs

--- main.py
+++ main.py
@@ -8,6 +8,14 @@ from langchain.schema import (
     HumanMessage,
     AIMessage
 )
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from htmlTemplates import css, bot_template, user_template
+
 prompt = "I want you to act as an accountant and come up with creative ways to manage finances. You'll need to consider budgeting, investment strategies and risk management when creating a financial plan for your client. In some cases, you may also need to provide advice on taxation laws and regulations in order to help them maximize their profits."
 
 def init():
@@ -19,25 +27,70 @@ def init():
         page_title="PDF Q&A - PoC",
         page_icon="🐱‍🐉"
     )
-    
+    st.write(css, unsafe_allow_html=True)
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
 
-def main(): 
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+def get_vectorstore(text_chunks):
+    embeddings = OpenAIEmbeddings()
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectorstore
+
+def get_conversation_chain(vectorstore):
+    llm = ChatOpenAI()
+
+    memory = ConversationBufferMemory(
+        memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+         memory=memory
+     )
+    return conversation_chain
+
+def handle_userinput(user_question):
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chat_history = response['chat_history']
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+
+
+def main():
+
     init()
-    if "messages" not in st.session_state:
-        st.session_state.messages = [
-            SystemMessage(content=prompt)
-        ]
-    if 'last_input' not in st.session_state:
-        st.session_state.last_input = ''
-
-    chat = ChatOpenAI(temperature=0.5)#nem kell api kulcs mert langchain automatikusan kiszedi .env-bol
-
-    messages = [
-        SystemMessage(content=prompt) #Instruction
-    ]
 
 
     st.header("Chat with yout pdfs")
+    user_question = st.text_input("Ask a question about your documents:")
+    if user_question:
+        handle_userinput(user_question)
 
     with st.sidebar:
         st.subheader("Your documents")
@@ -45,31 +98,19 @@ def main():
             "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
         if st.button("Process"):
             with st.spinner("Processing"):
-                st.write("TODO")
-    
-    
+                # get pdf text
+                raw_text = get_pdf_text(pdf_docs)
 
-    user_input = st.text_input("Your question",  key="user_input")
+                # get the text chunks
+                text_chunks = get_text_chunks(raw_text)
 
-    if user_input and user_input != st.session_state.last_input:
-        st.session_state.last_input = user_input
-        st.session_state.messages.append(HumanMessage(content=user_input))
-        with st.spinner("Thinking"):
-            response = chat(st.session_state.messages)
-        st.session_state.messages.append(AIMessage(content=response.content))
+                # create vector store
+                vectorstore = get_vectorstore(text_chunks)
 
+                # create conversation chain
+                st.session_state.conversation = get_conversation_chain(
+                    vectorstore)
 
-    messages = st.session_state.get('messages',[]) # kiszedjuk a messageket
-    for i, msg in enumerate(messages):
-        if msg.type == "human":
-            message(msg.content,is_user=True, key=str(i)+"_usr")
-        elif msg.type == "ai":
-            message(msg.content,is_user=False, key=str(i)+"_ai")
-    st.write(messages)
-
-        
-
-                
 
 if __name__ == "__main__":
     main()
